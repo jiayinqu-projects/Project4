edde8f9f390eaa3e145daf20358fcc0458486f5a
quxxx179 <quxxx179@ebm-er-10-131-194-151.wireless.umn.edu>
2020-02-13 14:51:42 -0600

    Simplified the code

--- R/week4.R
+++ R/week4.R
@@ -6,7 +6,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 week4_df <- read_delim("../data/week4.dat", "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 str(week4_df)
-week4_df <- separate(week4_df, qs, c("q1", "q2", "q3", "q4", "q5"), sep = " - ")
+week4_df <- separate(week4_df, qs, c(paste0("q", 1:5)), sep = " - ")
 week4_df[,c(5:9)] <- sapply(week4_df[,c(5:9)], as.integer)
 week4_df[,c(5:9)][which(week4_df[,c(5:9)] == 0, arr.ind = TRUE)] <- NA
 
